88ab03fecca3ee2a4556f731db5efceefe4fd49a
jaodasilvacaguei-hue <jaodasilvacaguei@gmail.com>
2025-12-28 00:44:09 -0300

    Add ids.lua with predefined ID and reason pairs

---
+++ ids.lua
@@ -0,0 +1,5 @@
+return {
+    { id = "1240432115800211457", reason = "Nigga" },
+    { id = "1447977804905058391", reason = "Suck My Dick" },
+    { id = "1058444079945486536", reason = "Suck My Dick" }
+}
