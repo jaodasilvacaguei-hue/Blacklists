81160560a43a01678d61180be8671b448e57ea23
jaodasilvacaguei-hue <jaodasilvacaguei@gmail.com>
2025-12-28 00:54:50 -0300

    Remove test entry from ids.lua
    Removed an entry with id '1308669328253517887' from the list.

--- ids.lua
+++ ids.lua
@@ -1,5 +1,4 @@
 return {
-    { id = "1308669328253517887", reason = "Test" },
     { id = "1240432115800211457", reason = "Nigga" },
     { id = "1447977804905058391", reason = "Suck My Dick" },
     { id = "1058444079945486536", reason = "Suck My Dick" }
