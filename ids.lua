73cb33fabc993d23f5b8296588f657624b7f6153
jaodasilvacaguei-hue <jaodasilvacaguei@gmail.com>
2025-12-28 03:53:24 -0300

    Update ids.lua

--- ids.lua
+++ ids.lua
@@ -1,5 +1,9 @@
 return {
-    { id = "1240432115800211457", reason = "Nigga" },
+    { id = "541253793405337600", reason = "Fxis Gay"},
+    { id = "1240432115800211457", reason = "Suck My Dick" },
+    { id = "1452665548943921287", reason = "Suck My Dick" },
+    { id = "1393951530767482948", reason = "Suck My Dick" }
+    { id = "1326000567213883422", reason = "Suck My Dick" },
     { id = "1447977804905058391", reason = "Suck My Dick" },
-    { id = "1058444079945486536", reason = "Suck My Dick" }
+    { id = "1139494865873403964", reason = "Suck My Dick" }
 }
