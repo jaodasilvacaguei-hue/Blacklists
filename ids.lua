ae026d3c79db11cb19a18f94887f94758e3511a8
jaodasilvacaguei-hue <jaodasilvacaguei@gmail.com>
2025-12-29 14:02:03 -0300

    Sync blacklist

--- ids.lua
+++ ids.lua
@@ -1,14 +1,14 @@
 return {
-    { id = "541253793405337600", reason = "Fxis Gay"},
+    { id = "541253793405337600", reason = "Fxis Gay" },
     { id = "1448769037134856253", reason = "Not Safe" },
     { id = "1349041898148335618", reason = "Good Try Nigger" },
-    { id = "1289604641742524570", reason = "Quebra De Linha Com Break"},
-    { id = "1379259528281067561", reason = "Quebra De Linha Com Break"},
+    { id = "1289604641742524570", reason = "Quebra De Linha Com Break" },
+    { id = "1379259528281067561", reason = "Quebra De Linha Com Break" },
     { id = "1112886240254103602", reason = "Tomar Key Lifetime Ai" },
     { id = "1240432115800211457", reason = "Suck My Dick" },
     { id = "1452665548943921287", reason = "Suck My Dick" },
-    { id = "1393951530767482948", reason = "Suck My Dick" }
+    { id = "1393951530767482948", reason = "Suck My Dick" },
     { id = "1326000567213883422", reason = "Suck My Dick" },
     { id = "1447977804905058391", reason = "Suck My Dick" },
-    { id = "1139494865873403964", reason = "Suck My Dick" }
+    { id = "1139494865873403964", reason = "Suck My Dick" },
 }
