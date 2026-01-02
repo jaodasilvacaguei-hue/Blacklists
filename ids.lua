2dd45909cf439d8943af952c3526c505bc19e698
jaodasilvacaguei-hue <jaodasilvacaguei@gmail.com>
2026-01-02 13:29:30 -0300

    Remove 1440202581799473274 from blacklist

--- ids.lua
+++ ids.lua
@@ -15,5 +15,4 @@ return {
     { id = "884161512179138570", reason = "HAHHAHAHAH FUCK YOU NIGGER" },
     { id = "1365428218873647134", reason = "Negrito" },
     { id = "1448211884862869504", reason = "Staff Team and tester" },
-    { id = "1440202581799473274", reason = "fivem test server -strxsi" },
 }
